7f3d9894e0fd8e0d7cf39512b8dfaa9f8d3733c3
droy <deepanwita23297@gmail.com>
2020-09-18 13:18:38 +0800

    added components

--- angular/new-app/src/app/app.module.ts
+++ angular/new-app/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {Left1Component} from './left-side/left1/left1.component'
+import {Left2Component} from './left-side/left2/left2.component'
+import {LeftSideModule} from './left-side/left-side.module'
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule
+    // , LeftSideModule
   ],
   providers: [],
   bootstrap: [AppComponent]
