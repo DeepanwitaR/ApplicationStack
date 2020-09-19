58c8ec10d5d05628474d3690924ebb1c7f1aa216
droy <deepanwita23297@gmail.com>
2020-09-19 21:30:07 +0800

    routing added

--- angular/new-app/src/app/app.module.ts
+++ angular/new-app/src/app/app.module.ts
@@ -2,13 +2,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {LeftSideModule} from './left-side/left-side.module'
 import { AppComponent } from './app.component';
-
+import {RouterModule} from '@angular/router';  
+import { Left1Component } from './left-side/left1/left1.component';
+import { Left2Component } from './left-side/left2/left2.component';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, LeftSideModule
+    BrowserModule, 
+    LeftSideModule,
+    RouterModule.forRoot([
+      { 
+        path: '', 
+        component: AppComponent
+      },
+      { 
+        path: 'left1', 
+        component: Left1Component
+      }, //path is and their corresponding component.
+      {
+        path: 'left2', 
+        component: Left2Component
+      },
+      { 
+        path: '**', 
+        component: Left1Component
+      }
+      // {
+      //   path: 'left2/:parameter_dynamicallyput', 
+      //   component: Left2Component
+      // }
+      // {
+    ]) //root routes, where we hit the //we must have a routerOutlet, where to display the corresponding comp.
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular/new-app/src/app/left-side/left-side.module.ts
+++ angular/new-app/src/app/left-side/left-side.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Left1Component } from './left1/left1.component';
 import { Left2Component } from './left2/left2.component';
-
+import {RouterModule} from '@angular/router';
 @NgModule({
   declarations: [
      Left1Component, Left2Component
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot([
+      { 
+        path: '', 
+        component: Left1Component
+      }, //path is and their corresponding component.
+      {
+        path: 'left2', 
+        component: Left2Component
+      }
+      // {
+      //   path: 'left2/:parameter_dynamicallyput', 
+      //   component: Left2Component
+      // }
+      // {
+      //   path: '**', 
+      //   component: NotFoundComponent
+      // } //wildcard
+    ]) //root routes, where we hit the //we must have a routerOutlet, where to display the corresponding comp.
   ],
   exports: [
     CommonModule
